4118d24d444a216f4ad3f7ef98127de98e37d7ce
pilot99fsa <132755045+pilot99fsa@users.noreply.github.com>
2023-12-17 20:53:33 +0900

    復習ログ追加

---
+++ JavaScript_Tutorial/アロー関数/アロー関数とthis/index.js
@@ -0,0 +1,24 @@
+// 無名関数ではthisという値がセットされるが、アロー関数ではセットされない。挙動の違いを見ていこう
+
+window.name = 'John'
+
+const person1 = {
+    name: 'Tom',
+    hello1: function () {
+        console.log('Hello' + this.name)
+    }
+}
+person1.hello1()
+//当然だがコンソールにはHello Tomと表示される
+
+// 名前を変えただけでほぼ同じ内容のperson2オブジェクトを作り、hello2の関数をアロー関数に置き換える
+const person2 = {
+    name: 'Tom',
+    hello2: () => {
+        console.log('Hello' + this.name)
+    }
+}
+person2.hello2()
+//コンソールにはHello Johnと表示される
+// アロー関数はthisを取らない
+// レキシカルスコープに探しに行き、windowオブジェクトを見つけた為、Johnと表示される
