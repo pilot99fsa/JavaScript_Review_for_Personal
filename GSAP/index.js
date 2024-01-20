34b9b4e8bad7e1ee8439ec1588e151c81bab252a
pilot99fsa <132755045+pilot99fsa@users.noreply.github.com>
2024-01-20 20:41:07 +0900

    カーソルが映らないミスを修正

--- GSAP/index.js
+++ GSAP/index.js
@@ -2,8 +2,15 @@ document.addEventListener("mousemove", (e) => {
     const mouseX = e.clientX; // clientXは、マウスのX座標の動きを取得するプロパティ
     const mouseY = e.clientY; // clientYは、マウスのY座標の動きを取得するプロパティ
 
+    // 文字の後ろのアニメーション
     gsap.to(".circle", { // 第1引数のcircleは14~16行目のクラス
         x: mouseX,
         y: mouseY,
+        stagger: -0.1
     });
+    //カーソルのアニメーション
+    gsap.set(".cursor", {
+        x: mouseX,
+        y: mouseY,
+    })
 });
