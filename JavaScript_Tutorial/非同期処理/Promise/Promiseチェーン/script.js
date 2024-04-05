8a0709d4ae3e0d1eaa784ef7078cc0fbd790b002
pilot99fsa <132755045+pilot99fsa@users.noreply.github.com>
2024-04-05 15:13:01 +0900

    Promiseチェーンについて

--- JavaScript_Tutorial/非同期処理/Promise/Promiseチェーン/script.js
+++ JavaScript_Tutorial/非同期処理/Promise/Promiseチェーン/script.js
@@ -28,12 +28,34 @@ function sleep1(callback, val) {
 
 // このコードをPromiseを使って書き換えていく
 
-function sleep2(callback, val) {
-    // まず、Promiseのインスタンス化を行う、次に引数としてresolve,rejectを渡してやる
-    new Promise(function (resolve, reject) {
+function sleep2(val) {
+    // まず、Promiseのインスタンス化を行う、次に引数としてresolve,rejectを渡してやる(今回はresolveしか使わないので第二引数は省略しても良い)。
+    return new Promise(function (resolve, reject) { // returnを付け加えることで、thenメソッドを使うことができる
+        // setTimeout()をPromiseの中に配置してやる
+        setTimeout(function () {
+            console.log(val++)
+            // callbackをresolveに書き換える
+            resolve(val)
+        }, 1000)
     })
-    setTimeout(function () {
-        console.log(val++)
-        callback(val)
-    }, 1000)
 }
+// これで書き換えは完了したので、sleep2を実行してやる
+
+// 関数を実行した時点で、new Promiseが戻り値として返ってくるのでthenメソッドを追加してコールバック関数を付け加える
+sleep2(0).then(function (val) {
+    return sleep2(val)
+}).then(function (val) {
+    return sleep2(val)
+}).then(function (val) {
+    return sleep2(val)
+}).then(function (val) {
+    return sleep2(val)
+}).then(function (val) {
+    return sleep2(val)
+})
+// .then(function (val) {
+//     sleep2(val)
+//     return val
+// })
+
+// thenメソッドの中でPromiseチェーンを繋ぐ場合は、必ずコールバックの戻り値に、Promiseのインスタンスを返すことを忘れないこと(そう言う仕様になっている)
