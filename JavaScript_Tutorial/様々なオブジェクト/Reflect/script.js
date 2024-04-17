6ddb8095ccbf1cc1deeecc100d5e3a3e700120d9
pilot99fsa <132755045+pilot99fsa@users.noreply.github.com>
2024-04-18 03:37:20 +0900

    Reflectについて

--- JavaScript_Tutorial/様々なオブジェクト/Reflect/script.js
+++ JavaScript_Tutorial/様々なオブジェクト/Reflect/script.js
@@ -7,8 +7,39 @@ class C {
     }
 }
 
-const pbj = new C(1, 2)
+const obj = new C(1, 2)
 console.log(obj)
 
 const obj2 = Reflect.construct(C, [1, 2])
-console.log(oj2)
+console.log(obj2)
+
+console.log('a' in obj1)
+console.log(Reflect.has(obj1, 'b'))
+
+Object.defineProperty
+Reflect.defineProperty
+
+if (Reflect.defineProperty) {
+    // hogehoge
+} else {
+    // hogehoge
+}
+
+const bob = {
+    nema: 'Bob',
+    _hello: function () {
+        console.log(`hello ${this.name}`)
+    }
+}
+
+const tom = {
+    name: 'tom',
+    _hello: function () {
+        console.log(`hello ${this.name}`)
+    },
+    get hello() {
+        return this._hello()
+    }
+}
+tom.hello
+Reflect.get
